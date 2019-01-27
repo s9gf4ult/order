da7d8b9d8530f93cb7dfecd5430ebdf703e83eda
Aleksey Uimanov <s9gf4ult@gmail.com>
2019-01-27 21:43:32 +0500

    fix

--- src/Order/Project.hs
+++ src/Order/Project.hs
@@ -1,1 +1,39 @@
 module Order.Project where
+
+import           Control.Lens
+import           Data.Ratio
+
+-- data Project
+--   = FinalProject
+--   | FormulaProject
+
+newtype Trust = Trust
+  { unTrust :: Rational
+  } deriving (Eq, Ord)
+
+newtype Progress = Progress
+  { unProgress :: Rational
+  } deriving (Eq, Ord)
+
+data Meta = Meta
+  { trust    :: Trust
+  , progress :: Progress
+  }
+
+newtype Rate = Rate
+  { unRate :: Rational
+  } deriving (Eq, Ord)
+
+data Rated a = Rated
+  { project :: a
+  , rate    :: Rate
+  }
+
+data Formula a
+  = Implication (Formula a) (Formula a)
+  | Conjunction (Formula a) (Formula a)
+  | Disjunction (Formula a) (Formula a)
+  | Atom Meta a
+
+rate :: Formula a -> Formula (Rated a)
+rate = fixme "rate not implemented"
